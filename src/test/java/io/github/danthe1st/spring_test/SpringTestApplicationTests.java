7f950c0f8dc148d9b4b80152231c049afa6029a7
danthe1st <daniel@wwwmaster.at>
2023-07-20 15:36:47 +0200

    use DataJpaTest for simplicity

--- src/test/java/io/github/danthe1st/spring_test/SpringTestApplicationTests.java
+++ src/test/java/io/github/danthe1st/spring_test/SpringTestApplicationTests.java
@@ -2,9 +2,9 @@ package io.github.danthe1st.spring_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class SpringTestApplicationTests {
 	@Autowired
 	private SomeEntityRepository repo;
@@ -13,5 +13,4 @@ class SpringTestApplicationTests {
 	void someQuery() {
 		System.out.println(repo.getAllByOther_Something(false));
 	}
-
 }
