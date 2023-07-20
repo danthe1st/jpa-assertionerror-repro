36c904d4cfafa16201b8847b236b3070d34ab261
danthe1st <daniel@wwwmaster.at>
2023-07-20 15:37:37 +0200

    remove unnecessary listener

--- src/main/java/io/github/danthe1st/spring_test/SpringTestApplication.java
+++ src/main/java/io/github/danthe1st/spring_test/SpringTestApplication.java
@@ -4,21 +4,14 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.data.repository.CrudRepository;
 
 @SpringBootApplication
-public class SpringTestApplication implements ApplicationListener<ApplicationReadyEvent> {
+public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
-
-	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-	}
-
 }
 
 interface SomeEntityRepository extends CrudRepository<SomeEntity, SomeEntity.IdHolder> {
